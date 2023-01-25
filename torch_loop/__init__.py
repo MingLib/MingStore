99b206c6be1d3f5bc6952132f25bad520838a461
MingLib <xiaoming20001201@gmail.com>
2023-01-25 21:52:16 +0800

    Remove old version code
    Remove old version code, will update new code later

--- torch_loop/__init__.py
+++
@@ -1,5 +0,0 @@
-from .data_processor import prepare_data
-from .torch_loop import train
-from .utils import path_manager, set_log_file
-from .torch_loop import recognize_evaluate
-from .metirc import lfw_cross_validation
